afb224fdf4c33fa67905f9e89f244bbad9026782
Brian Lu <54346213+BrianLu32@users.noreply.github.com>
2020-05-01 21:56:52 -0500

    Add files via upload

--- collab.py
+++ collab.py
@@ -6,34 +6,30 @@ import matplotlib.pyplot as plt
 from sklearn.neighbors import NearestNeighbors
 from scipy.sparse import csr_matrix
 
-recipes = pd.read_csv('data/core-data-train_rating.csv')
-recipes = recipes[['user_id', 'recipe_id', 'rating']]
-recipes.dropna(inplace = True) # drop any NaN if any
-recipes = recipes[:50000]
+# Retrieve the data
+combinedData = pd.read_csv('data/combinedRecipes.csv')
+combinedData.dropna(inplace = True) # Drop any rows that contain NaN
+combinedData = combinedData[:50000] # Take in only the first 50,000 data
 
-coreRecipes = pd.read_csv('data/core-data_recipe.csv')
-coreRecipes = coreRecipes[['recipe_id', 'recipe_name']]
-coreRecipes.dropna(inplace = True)
+# Drop any duplicates of user_id and recipe_name
+combinedData = combinedData.drop_duplicates(['user_id', 'recipe_name'])
 
-combinedRecipes = pd.merge(recipes, coreRecipes, on = 'recipe_id')
+# Pivot the data around the recipe_name and the user_id along with its ratings
+combinedData = combinedData.pivot(index = 'recipe_name', columns = 'user_id', values = 'rating').fillna(0)
 
-combinedRecipes = combinedRecipes.drop_duplicates(['user_id', 'recipe_name'])
-
-combinedRecipes = combinedRecipes.pivot(index = 'recipe_name', columns = 'user_id', values = 'rating').fillna(0)
-
-recipeMatrix = csr_matrix(combinedRecipes.values)
+# Convert the pivot table into a matrix
+recipeMatrix = csr_matrix(combinedData.values)
 
 # knn
 recipeRecomm = NearestNeighbors(metric = 'cosine', algorithm = 'brute')
 recipeRecomm.fit(recipeMatrix)
 
 # testing 
-randomChoice = np.random.choice(combinedRecipes.shape[0])
-print(combinedRecipes.shape[0])
-distances, indices = recipeRecomm.kneighbors(combinedRecipes.iloc[randomChoice].values.reshape(1, -1), n_neighbors = 11)
+randomChoice = np.random.choice(combinedData.shape[0])
+distances, indices = recipeRecomm.kneighbors(combinedData.iloc[randomChoice].values.reshape(1, -1), n_neighbors = 11)
 
 for i in range(0, len(distances.flatten())):
     if i == 0:
-        print("Recommendations for Recipe: '{0}' on priority basis:\n".format(combinedRecipes.index[randomChoice]))
+        print("Recommendations for Recipe: '{0}' on priority basis:\n".format(combinedData.index[randomChoice]))
     else:
-        print('{0}: {1}'.format(i, combinedRecipes.index[indices.flatten()[i]]))
+        print('{0}: {1}'.format(i, combinedData.index[indices.flatten()[i]]))
